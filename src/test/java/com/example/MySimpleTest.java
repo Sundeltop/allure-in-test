cffc7a68ce4bef441e9b6e93341984e92684aaea
Oleksandr Nelipa <oleksandr.nelipa1@gmail.com>
2024-06-30 19:35:28 +0300

    Integration with Allure Selenide

--- src/test/java/com/example/MySimpleTest.java
+++ src/test/java/com/example/MySimpleTest.java
@@ -1,11 +1,22 @@
 package com.example;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MySimpleTest {
+
+    @BeforeAll
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+    }
 
     @Test
     void openGooglePageTest() {
