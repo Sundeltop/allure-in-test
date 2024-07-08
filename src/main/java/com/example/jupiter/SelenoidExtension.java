0ad9c425a07e8c15f82d34fb04a0ea0582e4de6e
Oleksandr Nelipa <oleksandr.nelipa1@gmail.com>
2024-07-08 13:01:34 +0300

    added selenoid to workflow

---
+++ src/main/java/com/example/jupiter/SelenoidExtension.java
@@ -0,0 +1,29 @@
+package com.example.jupiter;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+public class SelenoidExtension implements BeforeAllCallback {
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) {
+        extensionContext
+                .getRoot()
+                .getStore(GLOBAL)
+                .getOrComputeIfAbsent(this.getClass(), key -> {
+                    beforeAll();
+                    return null;
+                });
+    }
+
+    private void beforeAll() {
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", false, "enableVideo", false, "enableLog", false));
+    }
+}
--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -1,27 +1,19 @@
 package com.example;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.jupiter.AllureEnvironmentExtension;
+import com.example.jupiter.AllureSelenideExtension;
 import com.example.jupiter.AssertFailAllureScreenshotExtension;
+import com.example.jupiter.SelenoidExtension;
 import com.example.pages.GooglePage;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith({AssertFailAllureScreenshotExtension.class, AllureEnvironmentExtension.class})
+@ExtendWith({AssertFailAllureScreenshotExtension.class, AllureEnvironmentExtension.class,
+        AllureSelenideExtension.class, SelenoidExtension.class})
 public class GoogleTest {
-
-    @BeforeAll
-    static void setupAllureReports() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true)
-        );
-    }
 
     @Test
     void verifySearchResultIsDisplayed() {
